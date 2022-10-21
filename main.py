cbe0950eaf3b02f8d5d59202e88802ffd4865487
Vladimir Kireev <kireevvv@icloud.com>
2022-10-21 13:10:24 +0600

    database created

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ bot = telebot.TeleBot('5707824022:AAHZzhzXSm_kMQkzg8n2tVTBXEHn3qh27JM')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == 'Привет':
-        bot.send_message(message.from_user.id, 'Привет, чем я могу тебе помочь?')
+    if message.text == '/lowprice':
+        bot.send_message(message.from_user.id, 'тут пойдет исполнение команды LP')
+    elif message.text == '/highprice':
+        bot.send_message(message.from_user.id, 'тут пойдет исполнение команды HP')
+    elif message.text == '/bestdeal':
+        bot.send_message(message.from_user.id, 'тут пойдет исполнение команды BD')
+    elif message.text == '/history':
+        bot.send_message(message.from_user.id, 'тут пойдет исполнение команды history')
     elif message.text == '/help':
-        bot.send_message(message.from_user.id, 'Напиши привет')
+        bot.send_message(message.from_user.id, 'Список команд:\n'
+                                               '/lowprice - вывод самых дешевых отелей в городе\n'
+                                               '/highprice - вывод самых дорогих отелей в городе\n'
+                                               '/bestdeal - вывод отелей, наиболее подходящих по цене и расположению от центра\n'
+                                               '/history - вывод истории поиска отелей\n')
     else:
-        bot.send_message(message.from_user.id, 'Я тебя не понимаю. Напиши /help')
+        bot.send_message(message.from_user.id, 'Я тебя не понимаю. Чтобы узнать список команд напиши /help')
 
 bot.polling(none_stop=True, interval=0)
 
