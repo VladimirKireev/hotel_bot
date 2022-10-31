45f26f777a8fe75875ba27de32ceb43ae4a666ad
Vladimir Kireev <kireevvv@icloud.com>
2022-10-31 10:03:59 +0600

    fix error when city is not exist

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ bot = telebot.TeleBot('5707824022:AAHZzhzXSm_kMQkzg8n2tVTBXEHn3qh27JM')
 
 class Destination:
 
-    def __init__(self, city):
+    def __init__(self, city, destination_id):
         self.city = city
+        self.destination_id = destination_id
         self.hotel_count = 0
         self.photos_count = 0
 
@@ -18,18 +19,21 @@ user_dict = dict()
 @bot.message_handler(commands=['lowprice'])
 def get_text_messages(message):
     msg = bot.reply_to(message, 'В какой город вы собираетесь?')
-
-
     bot.register_next_step_handler(msg, city_pick_step)
 
 
 def city_pick_step(message):
     city = message.text
-    user_id = message.from_user.id
-    user_dict[user_id] = Destination(city)
-    print(user_dict[user_id].city)
-    msg = bot.reply_to(message, 'Cколько вывести отелей в списке?')
-    bot.register_next_step_handler(msg, hotel_count_step)
+    try:
+        desnination_id = search_city(city)
+        user_id = message.from_user.id
+        user_dict[user_id] = Destination(city, desnination_id)
+        print(user_dict[user_id].city)
+        msg = bot.reply_to(message, 'Cколько вывести отелей в списке?')
+        bot.register_next_step_handler(msg, hotel_count_step)
+    except IndexError:
+        bot.reply_to(message, 'Не могу найти такой город. Вам необходимо заново выбрать команду и осуществить поиск.')
+
 
 
 def hotel_count_step(message):
@@ -60,7 +64,7 @@ def need_photo_step(message):
     else:
         city = user_dict[user_id].city
         hotel_count = user_dict[user_id].hotel_count
-        desnination_id = search_city(city)
+        desnination_id = user_dict[user_id].destination_id
 
         res = hotel_list(desnination_id, hotel_count)
         for i in res['hotels_info']:
@@ -77,7 +81,7 @@ def photo_count_step(message):
     city = user_dict[user_id].city
     photo_count = int(message.text)
     hotel_count = user_dict[user_id].hotel_count
-    desnination_id = search_city(city)
+    desnination_id = user_dict[user_id].destination_id
     res = hotel_list(desnination_id, hotel_count, photo_count)
     for i in res['hotels_info']:
         # print(i)
