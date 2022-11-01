a5fac8a876b68005552d338fb10e3f812e696f6c
Vladimir Kireev <kireevvv@icloud.com>
2022-11-01 16:17:12 +0600

    last update. main.py works, test.py in progress

--- handlers/default_heandlers/help.py
+++ handlers/default_heandlers/help.py
@@ -2,6 +2,19 @@ from telebot.types import Message
 from python_basic_diploma.loader import bot
 
 
-@bot.message_handler(commands=['help'])
+print('это команда хелп')
+# bot.send_message(258281993, 'тест хелп')
+
+# @bot.message_handler(content_types=['text'])
+# def get_text_messages(message):
+#     print(message.text)
+
+
+@bot.message_handler(content_types=["text"])
 def bot_help(message: Message):
     bot.send_message(message.from_user.id, 'Это команда помощи?')
+
+#
+# @bot.message_handler(commands=['help'])
+# def get_text_messages(message: Message):
+#     bot.reply_to(message, 'Помочь?')
--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 from loader import bot
-import python_basic_diploma.handlers
+from python_basic_diploma import handlers
 from utils.set_bot_commands import set_default_commands
 
 
@@ -7,7 +7,14 @@ if __name__ == '__main__':
     set_default_commands(bot)
 
 
+
     # @bot.message_handler(commands=['help'])
     # def get_text_messages(message):
     #     bot.reply_to(message, 'Помочь?')
+
+    # @bot.message_handler(commands=['start'])
+    # def bot_start(message):
+    #     bot.send_message(message.from_user.id, f"Привет, {message.from_user.full_name}!\n"
+    #                                            f"Чтобы узнать, что я могу, введи команду /help")
+
     bot.infinity_polling()
